0d88ff41882a2e3f336548d52570660a32c3b2fd
Michelle Anderson <manderson0102@yahoo.com>
2026-07-21 16:38:14 -0400

    iterations

--- questions/twoSum/index.js
+++ questions/twoSum/index.js
@@ -8,7 +8,23 @@
  * twoSum([10, 20], 30) -> [0, 1]
  * twoSum([3, 2, 4], 6) -> [1, 2]
  * 
- */
+ / ABSOLUTE BRUTE FORCE SOLUTION
+
+  for (let i = 0; i < nums.length; ++i) {
+    console.log(`loop ${i}`);
+    for (let j = 0; j < nums.length; ++j) {
+        console.log(nums[i], nums [j]);
+    }
+  }
+
+  //REMOVE LAST ITERATION**/
+
+  for (let i = 0; i < nums.length; ++i) {
+    console.log(`loop ${i}`);
+    for (let j = I + 1; j < nums.length; ++j) {
+        console.log(nums[i], nums [j]);
+    }
+  }
 
 const twoSum = (nums, target) => {
    
