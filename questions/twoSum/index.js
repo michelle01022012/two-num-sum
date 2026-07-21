a24b99a282819bf66a2669b37c6e2d99807aba4f
Michelle Anderson <manderson0102@yahoo.com>
2026-07-21 17:04:28 -0400

    jr, mid, sr level code

--- questions/twoSum/index.js
+++ questions/twoSum/index.js
@@ -12,22 +12,45 @@
 
   for (let i = 0; i < nums.length; ++i) {
     console.log(`loop ${i}`);
-    for (let j = 0; j < nums.length; ++j) {
-        console.log(nums[i], nums [j]);
-    }
+    for (let j = 0; j < nums.length; ++j)) {
+       if (mapDifferenceToIndex.has(nums[i])) {
+         return [mapDifferenceToIndex.get(nums[i]), i]
+       }
+         const differnce = target - nums[i]
+         mapDifferenceToIndex.set(difference, i);
+     }
   }
+  /TOP TECH SOLUTION  150 - 200K
+   const twoSum = (nums, target) => {
+    const mapDifferenceToIndex = New Map();
 
-  //REMOVE LAST ITERATION**/
+    for (let i = 0; i < nums.length; ++i){
+    }
+    }
 
+  //MID LEVEL SOLUTION** 80 - 100K
+  // 0(n^2) time | 0(1) space
   for (let i = 0; i < nums.length; ++i) {
-    console.log(`loop ${i}`);
+    const twoSum = (nums, target) => {
     for (let j = I + 1; j < nums.length; ++j) {
-        console.log(nums[i], nums [j]);
+        if (nums[i] + nums[j] === target) {
+            return [i, j]
+        }
+      }
     }
   }
 
-const twoSum = (nums, target) => {
-   
-}
+  //JUNIOR LEVEL SOLUTION** 60 - 80K
+ // const twoSum = (nums, target) => {
+ // for (let i = 0; i < nums.length; ++i) {
+ //   console.log(`loop ${i}`);
+ // for (let j = I + 1; j < nums.length; ++j) {
+ //   if (nums[i] + nums[j] === target){
+ //     return [i, j]
+ //   }
+ // }
+    
+  
 
+//DO NOT EDIT BELOW THIS LINE
 module.exports = twoSum;
