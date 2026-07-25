be1a52448180006841ca98e08cd796dee72ef607
Michelle Anderson <manderson0102@yahoo.com>
2026-07-25 04:14:14 -0400

    updated code for solution

--- questions/productExceptSelf/index.js
+++ questions/productExceptSelf/index.js
@@ -14,7 +14,45 @@
  */
 
 const productExceptSelf = (nums) => {
+  const results = []
 
+  results[0] = 1
+
+  for (let i = 1; i < nums.length; ++i) {
+    results[i] = results[i - 1] * nums[i - 1]
+  }
+
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; --i) {
+    results[i] = results[i] * rightProduct
+    rightProduct *= nums[i]
+  }
+
+  return results
+};
+
+// BRUTEFORCE
+const productExceptSelf2 = (nums) => {
+  const leftProducts = [];
+  const rightProducts = [];
+  const result = [];
+
+  leftProducts[0] = 1;
+  rightProducts[nums.length - 1] = 1;
+
+  for (let i = 1; i < nums.length; ++i) {
+    leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+  }
+
+  for (let i = nums.length - 2; i >= 0; --i) {
+    rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+  }
+
+  for (let i = 0; i < nums.length; ++i) {
+    result[i] = leftProducts[i] * rightProducts[i];
+  }
+
+  return result;
 };
 
 module.exports = productExceptSelf;
