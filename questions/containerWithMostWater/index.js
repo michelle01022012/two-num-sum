c7754ea407ca0eb30b041fc7b3b769a3cf2da102
Michelle Anderson <manderson0102@yahoo.com>
2026-07-25 04:23:11 -0400

    updates to code base

--- questions/containerWithMostWater/index.js
+++ questions/containerWithMostWater/index.js
@@ -12,29 +12,26 @@
  */
 
  const maxWater = (heights) => {
-   let l = 0
-   let r = heights.length - 1
-   let maxArea = 0 
+   let l = 0;
+   let r = heights.length - 1;
+   let maxArea = 0;
 
-   while (l < r){
-      let width = r - l
-      let height = Math.min(heights[l], heights[r])
-      let area = width * height
+   while (l < r) {
+     let width = r - l;
+     let height = Math.min(heights[l], heights[r]);
+     let area = width * height;
 
-      if (heights[l] < heights[r]){
-         l += 1
-      }
-      else{
-         r -= 1
-      }
+     if (heights[l] < heights[r]) {
+       l += 1;
+     } else {
+       r -= 1;
+     }
 
-      if (area > maxArea){
-         maxArea = area
-      }
+     if (area > maxArea) {
+       maxArea = area;
+     }
    }
-
-   return maxArea
-
-}
+   return maxArea;
+ };
 
 module.exports = maxWater;
