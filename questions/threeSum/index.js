70de6fe95225db05a4e8e1fae22d89e013b4155d
Michelle Anderson <manderson0102@yahoo.com>
2026-07-25 04:13:11 -0400

    three sum code resolution updates

--- questions/threeSum/index.js
+++ questions/threeSum/index.js
@@ -11,8 +11,39 @@
  * threeSum([0, 0, 0, 0]) = [[0, 0, 0]]
  */
 
+// O(nlog(n)) + O(n^2) = O(n^2) time 
+// O(n) space
 function threeSum(nums) {
-  
+  nums.sort((a, b) => a - b); // O(nlog(n)) time | O(n) space
+  const results = [];
+
+  for (i = 0; i < nums.length - 2; ++i) { // O(n) * O(n) = O(n^2) time
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) { // O(n) time
+      const sum = nums[i] + nums[l] + nums[r];
+
+      if (sum > 0) {
+        r--;
+      }
+      if (sum < 0) {
+        l++;
+      }
+      if (sum === 0) {
+        results.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (nums[l] === nums[l - 1] && l < r) {
+          l++;
+        }
+      }
+    }
+  }
+
+  return results;
 }
 
 //DO NOT EDIT BELOW THIS LINE
