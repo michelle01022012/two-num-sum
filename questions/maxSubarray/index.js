b8d0a17fe2c814e275aebf01b01efb67f16f5f41
Michelle Anderson <manderson0102@yahoo.com>
2026-07-25 04:04:26 -0400

    max sub array code updates

--- questions/maxSubarray/index.js
+++ questions/maxSubarray/index.js
@@ -9,8 +9,51 @@
   * maxSubArray([2, 4, 3, -1, -4, -9]) = 9
   */
 
+// MOST OPTIMAL SOLUTION 
+// O(n) time | O(1) space
 const maxSubArray = (nums) => {
+   let maxSum = nums[0]
+   let sum = 0
 
+   for (let i = 0; i < nums.length; ++i) { // O(n) time
+      if (sum < 0) {
+         sum = 0
+      }
+      sum += nums[i]
+      maxSum = Math.max(maxSum, sum)
+   }
+
+   return maxSum
+}
+
+// O(n^2) time | O(1) space
+const maxSubArray2 = (nums) => {
+   let maxSum = nums[0];
+
+   for (let i = 0; i < nums.length; ++i) {  // O(n) time
+      let sum = 0;
+      for (let j = i; j < nums.length; ++j) { // O(n) time
+         sum += nums[j]
+         maxSum = Math.max(maxSum, sum);
+      }
+   }
+
+   return maxSum
+}
+
+// O(n^3) time | O(n) space
+const maxSubArray3 = (nums) => {
+   let maxSum = nums[0];
+
+   for (let i = 0; i < nums.length; ++i) { // O(n) * O(n^2) -> O(n^3)
+      for (let j = i + 1; j <= nums.length; ++j) { // O(n) * O(2n) -> O(n^2)
+         const subarray = nums.slice(i, j) // O(n) time & space
+         const sum = subarray.reduce((acc, curr) => acc + curr) // O(n)
+         maxSum = Math.max(maxSum, sum)
+      }
+   }
+
+   return maxSum
 }
 
 //DO NOT EDIT BELOW THIS LINE
