f38d0ea46e5923d87103393334941169ca118894
Michelle Anderson <manderson0102@yahoo.com>
2026-07-25 01:42:37 -0400

    updated sortedArr

--- questions/containsDuplicate/index.js
+++ questions/containsDuplicate/index.js
@@ -12,7 +12,7 @@
 
 // sorting solution 80k - 100 k
 const containsDuplicate = (nums) => {
-    const sortedArr = nums.sort((a,b)) => a - b)
+    const sortedArr = nums.sort((a,b) => a - b)
 
     for (let i = 0; i < sortedArr.length -1; ++i) {
         if (sortedArr[i] === sortedArr[i + 1]) {
