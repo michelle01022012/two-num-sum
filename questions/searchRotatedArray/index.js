e4b9b62f594fa45c4e68db66f38c3d07a8c1f4a7
Michelle Anderson <manderson0102@yahoo.com>
2026-07-25 04:19:35 -0400

    updated code

--- questions/searchRotatedArray/index.js
+++ questions/searchRotatedArray/index.js
@@ -8,12 +8,42 @@
  * 
  * @example
  * search([5, 4, 1, 2, 3], 2) -> 3
- * search([7, 9, 12, 3, 4], 7) -> 0
- * search([3, 4, 2, 0], 4) -> 1
+ * search([7, 9, 12, 3, 4], 7) -> 1
+ * search([3, 4, 2, 0], 3) -> 0
  * 
  */
 
 const search = (nums, target) => {
+    
+    let l = 0
+    let r = nums.length - 1
+
+    while (l <= r){
+        let mid = Math.floor((l + r) / 2)
+
+        if (nums[mid] === target) return mid
+
+        //In the green portion of the array
+
+        if (nums[mid] < nums[r]){
+            if (target < nums[mid] || target > nums[r]){
+                r = mid - 1
+            }
+            else{
+                l = mid + 1
+            }
+        }
+        //In the blue portion of the array
+        else {
+            if (target > nums[mid] || target < nums[l]){
+                l = mid + 1
+            }
+            else r = mid - 1
+        }
+
+    }
+
+    return "not found"
 
 }
 
